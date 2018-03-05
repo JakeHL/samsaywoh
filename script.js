24b8db347e946d5d8ea935b445913a0b71ab1558
unknown <jakelangford@outlook.com>
2018-03-05 20:46:57 +0000

    initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+var woh = document.getElementById("woh");
+
+woh.onclick = function() {
+    if(woh.mp3) {
+        if(woh.mp3.paused) woh.mp3.play();
+        else woh.mp3.pause();
+    }
+    else {
+        woh.mp3 = new Audio("/WOT.mp3");
+        woh.mp3.play();
+    }
+}
